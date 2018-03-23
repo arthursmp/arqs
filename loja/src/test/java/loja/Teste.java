81917ba57066be3475c64f06d44f8ffef9908507
116122106 <116122106@ESTB8LI0621.animaedu.intranet>
2018-03-22 22:32:03 -0300

    Commit versão 6 - teste unitário

--- loja/src/test/java/loja/Teste.java
+++ loja/src/test/java/loja/Teste.java
@@ -1,15 +1,24 @@
 package loja;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import br.unibh.loja.entidades.Produto;
+
 
 public class Teste {
 
-	public static void main(String[]args) {
+	@Test
+	public void teste1() {
 		
 		Produto p1 = new Produto(1L, "Caixa de sapato");
 		p1.toString();
 		Produto p2 = new Produto(2L, "Caixa de sapato");
 		p2.toString();
+		
+		
+		assertEquals(p1.equals(p2),false);
 		
 	}
 }
