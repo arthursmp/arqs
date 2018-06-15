cfe3884f3ce41cdd40b824bd0acc834a724306cc
116122106 <116122106@ESTB8LI0621.animaedu.intranet>
2018-06-14 21:06:19 -0300

    dada

--- loja/src/main/java/br/unibh/loja/integracao/RestProduto.java
+++ loja/src/main/java/br/unibh/loja/integracao/RestProduto.java
@@ -10,27 +10,29 @@ import javax.ws.rs.core.MediaType;
 
 import br.unibh.loja.entidades.Categoria;
 import br.unibh.loja.entidades.Cliente;
+import br.unibh.loja.entidades.Produto;
 import br.unibh.loja.negocio.ServicoCategoria;
 import br.unibh.loja.negocio.ServicoCliente;
+import br.unibh.loja.negocio.ServicoProduto;
 import io.swagger.annotations.Api;
 
 @Api
-@Path("cliente")
+@Path("produto")
 public class RestProduto {
 	@Inject
-	private ServicoCategoria sc;
+	private ServicoProduto sc;
 
 	@GET
 	@Path("list")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Categoria> helloworld() throws Exception {
+	public List<Produto> helloworld() throws Exception {
 		return sc.findAll();
 	}
 
 	@GET
 	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Categoria hello(@PathParam("id") final Long id) throws Exception {
+	public Produto hello(@PathParam("id") final Long id) throws Exception {
 		return sc.find(id);
 	}
 }
